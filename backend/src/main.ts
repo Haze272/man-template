20c9984bc7bd14a5de2c028b12bbf1b829b2baec
Stepan Berkunov <studiorain27@gmail.com>
2024-12-13 00:57:56 +0300

    build(backend): add backend configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import * as process from 'node:process';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  console.log(process.env.DATABASE_HOST ?? 'NO DB HOST');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept',
+  });
+  app.setGlobalPrefix(process.env.APP_API_URL ?? 'api/v1');
+  app.use(cookieParser());
+  await app.listen(parseInt(process.env.APP_PORT ?? 'api/v1'));
 }
 bootstrap();
