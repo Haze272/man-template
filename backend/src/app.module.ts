cba2870f95c2b62a87e17f211814c29667aa487f
Stepan Berkunov <studiorain27@gmail.com>
2024-12-13 00:05:58 +0300

    feat(backend): add main modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { IamModule } from './iam/iam.module';
+import { RolesModule } from './roles/roles.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [IamModule, RolesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
