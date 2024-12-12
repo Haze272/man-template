1912381efab3f398d487e0af861b8e2a38ba977a
Stepan Berkunov <studiorain27@gmail.com>
2024-12-13 00:08:19 +0300

    feat(backend): add env and db connection

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,30 @@ import { AppService } from './app.service';
 import { IamModule } from './iam/iam.module';
 import { RolesModule } from './roles/roles.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as process from 'node:process';
 
 @Module({
-  imports: [IamModule, RolesModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: 'src/.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    IamModule,
+    RolesModule,
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
